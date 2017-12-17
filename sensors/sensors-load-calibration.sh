f4e5ded43cd1192add3ebfa3c4bb104a591eaedb
Svyatoslav Ryhel <clamor95@gmail.com>
2017-12-17 10:43:11 +0200

    sensors: update calibration and permissions
    Change-Id: Ib79b6c7f708cbf4073cdf03202ea3d9b0a65656d

--- sensors/sensors-load-calibration.sh
+++ sensors/sensors-load-calibration.sh
@@ -5,7 +5,7 @@ if [ -f /per/lightsensor/AL3010_Config.ini ]; then
     cat /per/lightsensor/AL3010_Config.ini > /sys/devices/platform/tegra-i2c.2/i2c-2/2-001c/calibration
 fi
 
-# AMI304 Compass calibration data
-if [ -f /per/sensors/AMI304_Config.ini ]; then
-    cat /per/sensors/AMI304_Config.ini > /sys/devices/platform/tegra-i2c.2/i2c-2/2-000e/iio:device1/compass_cali_data
+# AMI306 Compass calibration data
+if [ -f /per/sensors/AMI306_Config.ini ]; then
+    cat /per/sensors/AMI306_Config.ini > /sys/devices/platform/tegra-i2c.2/i2c-2/2-000e/compass_cali_test
 fi

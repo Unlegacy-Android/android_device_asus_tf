c31fa2ead7dbe6d6b6f104b85ff20235aadff2fb
Svyatoslav Ryhel <clamor95@gmail.com>
2017-12-21 10:44:41 +0200

    wifi: remove p2p_supplicant
    bcm4329 does not support p2p (wifi direct)
    
    Change-Id: I93f9f5a3b9c8fd7a94ef45c283be34b0a0ad5b5b
    Signed-off-by: Svyatoslav Ryhel <clamor95@gmail.com>

--- device-common.mk
+++ device-common.mk
@@ -42,7 +42,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
-    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
 
@@ -65,6 +64,9 @@ PRODUCT_PACKAGES += \
     libstlport
 
 # Wi-Fi
+PRODUCT_COPY_FILES += \
+	device/asus/transformer/configs/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf
+
 PRODUCT_PACKAGES += \
     libwpa_client \
     hostapd \

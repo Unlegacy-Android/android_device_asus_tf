26a5257cdeb8c9f2dc244df5070d3ae6abc07fe2
Svyatoslav Ryhel <clamor95@gmail.com>
2018-08-18 22:34:22 +0300

    liblights: use new versioning system
    Use LIGHTS_DEVICE_API_VERSION_1_0 as a common basic version. Replace
    major and minor version with module and hal api versions.
    
    Change-Id: If4f2422f3723c57fffad61d7c805444c2444599b
    Signed-off-by: Svyatoslav Ryhel <clamor95@gmail.com>

--- liblights/lights.c
+++ liblights/lights.c
@@ -15,7 +15,6 @@
  */
 
 #define LOG_TAG "lights"
-#define LOGE ALOGE
 
 #include <cutils/log.h>
 
@@ -32,21 +31,29 @@
 #include <hardware/hardware.h>
 
 static pthread_mutex_t g_lock = PTHREAD_MUTEX_INITIALIZER;
+static int g_last_backlight_mode = BRIGHTNESS_MODE_USER;
+
+char const *const BRIGHTNESS_FILE
+        = "/sys/class/backlight/pwm-backlight/brightness";
+char const *const PERSISTENCE_FILE
+        = "/sys/class/backlight/pwm-backlight/low_persistence";
 
 static int write_int(char const *path, int value)
 {
 	int fd;
-	static int already_warned = -1;
-	fd = open(path, O_RDWR);
+	static int already_warned = 0;
+
+	fd = open(path, O_WRONLY);
 	if (fd >= 0) {
 		char buffer[20];
-		int bytes = sprintf(buffer, "%d\n", value);
-		int amt = write(fd, buffer, bytes);
+		size_t bytes = snprintf(buffer, sizeof(buffer), "%d\n", value);
+		if(bytes >= sizeof(buffer)) return -EINVAL;
+		size_t amt = write(fd, buffer, bytes);
 		close(fd);
 		return amt == -1 ? -errno : 0;
 	} else {
-		if (already_warned == -1) {
-			LOGE("write_int failed to open %s\n", path);
+		if (already_warned == 0) {
+			ALOGE("write_int failed to open %s\n", path);
 			already_warned = 1;
 		}
 		return -errno;
@@ -57,8 +64,8 @@ static int rgb_to_brightness(struct light_state_t const *state)
 {
 	int color = state->color & 0x00ffffff;
 	return ((77 * ((color >> 16) & 0x00ff))
-		+ (150 * ((color >> 8) & 0x00ff)) +
-		(29 * (color & 0x00ff))) >> 8;
+		+ (150 * ((color >> 8) & 0x00ff))
+		+ (29 * (color & 0x00ff))) >> 8;
 }
 
 static int set_light_backlight(struct light_device_t *dev,
@@ -66,36 +73,31 @@ static int set_light_backlight(struct light_device_t *dev,
 {
 	int err = 0;
 	int brightness = rgb_to_brightness(state);
-	//+++ power save
-	int range_num = 1;
-	int old_brightness_range_indexs[] = {160,255};
-	int new_brightness_range_indexs[] = {160,182};
-	int old_brightness = brightness;
-	int i=0;
-	int old_brightness_in_range = -1;// 0:1~10 , 1:10~102 , 2:102~255
-	for(i=0;i<range_num;i++){
-		int t_range_min = old_brightness_range_indexs[i];
-		int t_range_max = old_brightness_range_indexs[i+1];
-		if( brightness>=t_range_min && brightness<=t_range_max){
-			old_brightness_in_range = i;
-			break;
+	unsigned int lpEnabled = state->brightnessMode == BRIGHTNESS_MODE_LOW_PERSISTENCE;
+
+	if(!dev) {
+		return -1;
+	}
+
+	pthread_mutex_lock(&g_lock);
+
+	// If we're not in lp mode and it has been enabled or if we are in lp mode
+	// and it has been disabled send an ioctl to the display with the update
+	if ((g_last_backlight_mode != state->brightnessMode && lpEnabled) ||
+		(!lpEnabled && g_last_backlight_mode == BRIGHTNESS_MODE_LOW_PERSISTENCE)) {
+		if ((err = write_int(PERSISTENCE_FILE, lpEnabled)) != 0) {
+			ALOGE("%s: Failed to write to %s: %s\n", __FUNCTION__, PERSISTENCE_FILE,
+				strerror(errno));
 		}
 	}
-	if(old_brightness_in_range>=0){
-		int old_br_min = old_brightness_range_indexs[old_brightness_in_range];
-		int olb_br_max = old_brightness_range_indexs[old_brightness_in_range+1];
-		int new_br_min = new_brightness_range_indexs[old_brightness_in_range];
-		int new_br_max = new_brightness_range_indexs[old_brightness_in_range+1];
-		int new_brightness = new_br_min +
-				(new_br_max-new_br_min)*(old_brightness-old_br_min)/(olb_br_max-old_br_min);
-		brightness = new_brightness;
-	}
-	//---
-	pthread_mutex_lock(&g_lock);
-	err = write_int("/sys/class/backlight/pwm-backlight/brightness",
-			brightness);
-	pthread_mutex_unlock(&g_lock);
 
+	g_last_backlight_mode = state->brightnessMode;
+
+	if (!err) {
+		err = write_int(BRIGHTNESS_FILE, brightness);
+	}
+
+	pthread_mutex_unlock(&g_lock);
 	return err;
 }
 
@@ -111,7 +113,7 @@ static int close_lights(struct light_device_t *dev)
 static int open_lights(const struct hw_module_t *module, char const *name,
 		       struct hw_device_t **device)
 {
-	pthread_t lighting_poll_thread;
+//	pthread_t lighting_poll_thread;
 
 	int (*set_light) (struct light_device_t *dev,
 			  struct light_state_t const *state);
@@ -124,16 +126,19 @@ static int open_lights(const struct hw_module_t *module, char const *name,
 	pthread_mutex_init(&g_lock, NULL);
 
 	struct light_device_t *dev = malloc(sizeof(struct light_device_t));
+
+	if(!dev)
+		return -ENOMEM;
+
 	memset(dev, 0, sizeof(*dev));
 
 	dev->common.tag = HARDWARE_DEVICE_TAG;
-	dev->common.version = 0;
+	dev->common.version = LIGHTS_DEVICE_API_VERSION_2_0;
 	dev->common.module = (struct hw_module_t *)module;
 	dev->common.close = (int (*)(struct hw_device_t *))close_lights;
 	dev->set_light = set_light;
 
 	*device = (struct hw_device_t *)dev;
-
 	return 0;
 }
 

ab17e0ac403d72030e39d971959552482d95d778
Svyatoslav Ryhel <clamor95@gmail.com>
2018-08-20 21:04:25 +0300

    [HACK] build some needed HIDL HALs
    Based on grouper changes. Needed only now to boot. Will be overwritten
    after debug of audio and lights HALs.
    
    Change-Id: Ib0a30298f87d5936ec4a21d0df0d111e5a554329
    Signed-off-by: Svyatoslav Ryhel <clamor95@gmail.com>

--- device-common.mk
+++ device-common.mk
@@ -128,6 +128,12 @@ PRODUCT_COPY_FILES += \
     device/asus/transformer/media/media_profiles.xml:system/etc/media_profiles.xml \
     device/asus/transformer/media/media_codecs.xml:system/etc/media_codecs.xml
 
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-impl \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio.effect@2.0-impl
+
 # Vendor blobs
 $(call inherit-product, vendor/asus/transformer/asus-vendor.mk)
 $(call inherit-product, vendor/broadcom/transformer/broadcom-vendor.mk)

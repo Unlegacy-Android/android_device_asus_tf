9089e184bbd527c41c0dd141aa8e10a4984bc3ae
Svyatoslav Ryhel <clamor95@gmail.com>
2018-10-12 19:30:04 +0300

    Switch to permissive for testing purposes
    Change-Id: I476f29c3dddad408934dc73de71c9769a8dfe1e2
    Signed-off-by: Svyatoslav Ryhel <clamor95@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -41,7 +41,7 @@ MALLOC_SVELTE := true
 USE_CLANG_PLATFORM_BUILD := true
 
 # Kernel
-BOARD_KERNEL_CMDLINE := androidboot.hardware=$(TARGET_BOOTLOADER_BOARD_NAME)
+BOARD_KERNEL_CMDLINE := androidboot.hardware=$(TARGET_BOOTLOADER_BOARD_NAME) androidboot.selinux=permissive
 TARGET_KERNEL_CONFIG := transformer_defconfig
 TARGET_KERNEL_SOURCE := kernel/nvidia/tegra3
 KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
